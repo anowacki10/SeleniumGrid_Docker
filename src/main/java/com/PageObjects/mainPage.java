b28645298f8429d6f79d9e85112e0c24ed4848d2
anowacki10 <anowacki100@gmail.com>
2023-10-29 00:57:57 +0200

    app repository push

---
+++ src/main/java/com/PageObjects/mainPage.java
@@ -0,0 +1,69 @@
+package com.PageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class mainPage{
+
+
+        static By COOKIES = By.xpath("//*[@id=\"onetrust-accept-btn-handler\"]");
+        static By SEARCHER = By.xpath("//input[@id='headerSearch']");
+        static By LOCATION = By.xpath("//input[@id='cityField']");
+        static By SUBMIT = By.xpath("//input[@id='submit-searchmain']");
+
+
+        public static void COOKIES_click(WebDriver driver){
+            try {
+                WebElement cookies = driver.findElement(COOKIES);
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+                wait.until(ExpectedConditions.elementToBeClickable(cookies));
+                cookies.click();
+            }
+            catch (Exception e){
+                throw new RuntimeException("Something is wrong with cookies accept!");
+            }
+        }
+        public static void SEARCHER_input(WebDriver driver,String Searching_text){
+            try{
+            WebElement searcher = driver.findElement(SEARCHER);
+            WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+            wait.until(ExpectedConditions.visibilityOf(searcher));
+            searcher.sendKeys(Searching_text);
+            }
+            catch (Exception e){
+                throw new RuntimeException("Something is wrong!");
+            }
+
+        }
+        public static void LOCATION_input(WebDriver driver,String Location_text){
+            try{
+            WebElement location = driver.findElement(LOCATION);
+            WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+            wait.until(ExpectedConditions.visibilityOf(location));
+            location.sendKeys(Location_text);
+            }
+            catch (Exception e){
+                throw new RuntimeException("Something is wrong!");
+            }
+        }
+
+        public static void SUBMIT_click(WebDriver driver){
+            try{
+            WebElement submit = driver.findElement(SUBMIT);
+            WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+            wait.until(ExpectedConditions.elementToBeClickable(submit));
+            submit.click();
+            Thread.sleep(1000);
+            }
+            catch (Exception e){
+                throw new RuntimeException("Something is wrong!");
+            }
+        }
+
+
+}
---
+++ src/main/java/com/TestBase/WebDriverFactory.java
@@ -0,0 +1,81 @@
+package com.TestBase;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class WebDriverFactory {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    @Parameters({"browser", "gridURL", "appURL"})
+    public void setDriver(String browser, String gridURL, String appURL) throws MalformedURLException, InterruptedException {
+
+
+        if (browser.equalsIgnoreCase("chrome")) {
+
+
+            DesiredCapabilities chromeCap = new DesiredCapabilities();
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--incognito");
+            chromeOptions.addArguments("--headless");
+            chromeOptions.addArguments("--disable-gpu");
+            chromeCap.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+            chromeCap.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+            driver = new RemoteWebDriver(new URL(gridURL), chromeCap);
+            driver.manage().window().maximize();
+            driver.get(appURL);
+            Thread.sleep(2000);
+
+
+        } else if (browser.equalsIgnoreCase("firefox")) {
+
+            DesiredCapabilities firefoxCap = new DesiredCapabilities();
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.addArguments("--private");
+            //firefoxOptions.addArguments("--headless");
+            firefoxCap.setCapability(CapabilityType.BROWSER_NAME, "firefox");
+            firefoxCap.setCapability(FirefoxOptions.FIREFOX_OPTIONS, firefoxOptions);
+            driver = new RemoteWebDriver(new URL(gridURL), firefoxCap);
+            driver.manage().window().maximize();
+            driver.get(appURL);
+            Thread.sleep(2000);
+
+
+        }else if (browser.equalsIgnoreCase("edge")) {
+
+
+            EdgeOptions edgeOptions = new EdgeOptions();
+            edgeOptions.setCapability("ms:inPrivate", true);
+            edgeOptions.addArguments("headless");
+            driver = new RemoteWebDriver(new URL(gridURL), edgeOptions);
+            driver.manage().window().maximize();
+            driver.get(appURL);
+            Thread.sleep(3000);
+
+
+        }else {
+            throw new IllegalArgumentException("Invalid browser specified: " + browser);
+        }
+    }
+
+
+    @AfterMethod
+    public void tearDown() {
+            driver.quit();
+
+    }
+}
+
+
+
